d4fb9c9a55036859b1db658d06c2602e1f06844a
gaurav <gauravshirke895@gmail.com>
2024-07-14 17:08:53 +0530

    host added

--- application.py
+++ application.py
@@ -47,4 +47,5 @@ def predict():
     return str(np.round(prediction[0],2))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
